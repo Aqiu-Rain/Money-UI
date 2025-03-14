874007092d1603157ff36e6199761a9d5e14c4f0
Yijie-Wu <1694517106@qq.com>
2025-03-14 22:25:26 +0800

    update

--- src/stores/websocket.js
+++ src/stores/websocket.js
@@ -8,139 +8,66 @@ export const useWebSocketStore = defineStore('websocketStore', {
             status: 'Waiting Connect',
             message: '',
             socket: null,
-            heartbeatTimer: null,
-            lastUrl: null,
+            interval: null
         }
     },
     actions: {
         setData(data) {
             if (this.data.length >= 50) {
                 this.data = []
-                showMessage('warning', 'The page has reached the 50-entry setting limit. Previous data has been cleared, but you can view it in the history.')
+                // showMessage('warning', 'The page has reached the 50-entry setting limit. Previous data has been cleared, but you can view it in the history.')
             }
             this.data.unshift(data)
         },
         connect(url) {
-            this.lastUrl = url;
             this.socket = new WebSocket(url);
             this.socket.onopen = () => {
                 if (this.socket.readyState === WebSocket.OPEN) {
                     this.status = 'Connected'
-                    this.startHeartbeat();
+                    this.sendHeartbeat()
                 }
             };
             this.socket.onclose = () => {
-                if (this.socket.readyState === WebSocket.OPEN) {
+                clearInterval(this.interval)
+                if (this.socket.readyState === WebSocket.CLOSED) {
                     this.status = 'Waiting Connect'
                     this.socket.close()
                 }
             };
             this.socket.onerror = (e) => {
-                showNotification(
-                    'Error',
-                    e,
-                    'error',
-                    3000,
-                    'top-right',
-                )
-                try {
-                    this.socket.close();
-                    this.status = 'Waiting Connect'
-                    this.message = e
-                    this.socket = null
-                } catch (e) {
-                    this.status = 'Waiting Connect'
-                    this.message = e
-                    this.socket = null
-                } finally {
-                    this.status = 'Waiting Connect'
-                    this.socket = null
-                }
+                showNotification('Error', e, 'error', 3000, 'top-right')
+                this.socket.close()
+                this.status = 'Waiting Connect'
+                this.socket = null
             };
             this.socket.onmessage = (e) => {
                 const parsedData = JSON.parse(e.data);
+
                 if (parsedData.type === "serial_data") {
                     this.setData(parsedData.data)
                 } else if (parsedData.type === "notification") {
-                    showNotification(
-                        'Success',
-                        parsedData.data,
-                        'success',
-                        3000,
-                        'top-right',
-                    )
+                    showNotification('Success', parsedData.data, 'success', 3000, 'top-right')
                 } else if (parsedData.type === "error") {
-                    showNotification(
-                        'Error',
-                        parsedData.data,
-                        'error',
-                        3000,
-                        'top-right',
-                    )
-
-                    try {
-                        this.socket.close()
-                        this.socket = null
-                        this.status = 'Waiting Connect'
-                    } catch (e) {
-                        this.socket = null
-                        this.status = 'Waiting Connect'
-                    } finally {
-                        this.socket = null
-                        this.status = 'Waiting Connect'
-                    }
+                    showNotification('Error', parsedData.data, 'error', 3000, 'top-right')
+                } else if (parsedData.type === "heart") {
+                    console.log('Received heart: ' + parsedData.data)
                 }
             }
         },
         disconnect() {
-            try {
-                this.socket.close()
-                this.status = 'Waiting Connect'
-                this.socket = null
-            } catch (e) {
-                showNotification(
-                    'Error',
-                    'Disconnect error:' + e,
-                    'error',
-                    3000,
-                    'top-right',
-                )
-                this.status = 'Waiting Connect'
-                this.socket = null
-            } finally {
-                this.socket = null
-                this.status = 'Waiting Connect'
-            }
+            this.socket.disconnect();
+            this.status = 'Waiting Connect'
+            this.socket = null
+            clearInterval(this.interval)
         },
         sendData(data) {
             this.socket.send(JSON.stringify(data))
         },
-        ping() {
-            if (this.socket && this.socket.readyState === WebSocket.OPEN) {
-                this.socket.send(JSON.stringify({ type: 'heart', data: "ping" }));
-            }
-        },
-        startHeartbeat() {
-            this.heartbeatTimer = setInterval(() => {
-                if (this.socket && this.socket.readyState === WebSocket.OPEN) {
-                    this.ping();
-                } else {
-                    this.stopHeartbeat();
-                    this.reconnect();
-                }
-            }, 5000);
-        },
-        stopHeartbeat() {
-            if (this.heartbeatTimer) {
-                clearInterval(this.heartbeatTimer);
-                this.heartbeatTimer = null;
-            }
-        },
-        reconnect() {
-            if (this.socket) {
-                this.disconnect();
-            }
-            this.connect(this.lastUrl);
+        sendHeartbeat() {
+            this.interval = setInterval(() => {
+                console.log('Send heartbeat')
+                this.socket.send(JSON.stringify({cmd:'heart'}))
+            }, 5000)
         }
     }
 })
